18a8a98d0f74139e7ca7586641b5780246e27dd4
Emerson <emersonmendivil@gmail.com>
2026-05-27 10:05:13 -0500

    primera clase de datos estructurados

---
+++ 9.datos _estructurados/listas.py
@@ -0,0 +1,11 @@
+lista_vacia:list=[]
+print(len(lista_vacia))
+# por regla el nombre de una variable no deve teneral dato que se va almacenar 
+amores:list[str]=["wicho","pocohuanca","cesar","guido","percy"] 
+print(f"cantidad de elementos que tiene la variable amores es:{len(amores)}")
+frutas:list[str]=["🍎","🍍","🍐","🍒"] 
+#posision indice 
+# acceder al tercer elemento
+print(frutas[2])
+#acceser al segundo elemento 
+print(frutas[3])
